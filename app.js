74af1523e9f8ddc509ca5399ff18a0bca17d1d65
indrajit-work <indrajit.adhikary@brickviewstudios.com>
2021-07-11 18:39:29 +0530

    Logs

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const http = require('http');
 
 const server = http.createServer(function(req,res){
     if(req.url ==='/'){
+        console.log('request arrive');
         res.write('Hello World');
         res.end();
+        console.log('response sent');
     }
 
   
